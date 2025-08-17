3922ab0f560b4a3863c65764191f762c3a0640a5
m <Jasperbos1234@gmail.com>
2025-08-17 20:19:06 +0200

    start page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Nav from "./components/nav";
+import Socials from "./components/socials";
+
+export default function Home() {
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Nav/>
+      <Image alt="nyuh" src="/nyuh.png" height={1920} width={1920} className="select-none pointer-events-none my-2 w-[700px] h-auto"/>
+      <Socials/>
+      <hr className="my-5 rounded h-[5.5px] w-[500px] bg-white border-0"/>
+      <div className="mt-1 font-red-hat-display text-white text-[18px] tracking-[0.2rem] text-center">
+        <p className="mb-[0.6em]">she/her ⁃ 20yo</p>
+        <p className="mb-[0.6em]">discord: @nyuh.</p>
+        <p>♡</p>
+      </div>
+    </div>
+    <div className="flex justify-center mb-10"><Image alt="silly gif" src="/nyuh.gif" width={60} height={60}/></div>
+    </>
+  );
+}
