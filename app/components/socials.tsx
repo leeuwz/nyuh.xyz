45915955c792403f5a4890f495e105ea8d9343d2
m <Jasperbos1234@gmail.com>
2025-08-17 20:18:35 +0200

    add socials.tsx

---
+++ app/components/socials.tsx
@@ -0,0 +1,15 @@
+export default function Socials() {
+    return(
+        <div className="font-raleway flex items-center text-white">
+            <a href="https://osu.ppy.sh/users/12783604" target="_blank" className="text-[20px] underline tracking-[0.3em] hover:text-[#F7395B] transition duration-300">osu!</a>
+            <p className="mx-3">✦</p>
+            <a href="https://x.com/Nyuh__" target="_blank" className="text-[20px] underline tracking-[0.3em] hover:text-[#F7395B] transition duration-300">twitter</a>
+            <p className="mx-3">✦</p>
+            <a href="https://x.com/sleepynyuh" target="_blank" className="text-[20px] underline tracking-[0.3em] hover:text-[#F7395B] transition duration-300">art twitter</a>
+            <p className="mx-3">✦</p>
+            <a href="https://steamcommunity.com/id/Nyuh_" target="_blank" className="text-[20px] underline tracking-[0.3em] hover:text-[#F7395B] transition duration-300">steam</a>
+            <p className="mx-3">✦</p>
+            <a href="https://www.twitch.tv/nyuh___" target="_blank" className="text-[20px] underline tracking-[0.3em] hover:text-[#F7395B] transition duration-300">twitch</a>
+        </div>
+    )
+}
