2a7183de982f3fb132b5f43b1eca2b3dd2842aa5
m <Jasperbos1234@gmail.com>
2025-08-23 00:00:10 +0200

    create osuSkins.tsx

---
+++ app/sections/osuSkins.tsx
@@ -0,0 +1,10 @@
+export default function OsuSkins() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="font-red-hat-display text-white text-4xl mb-8">as</h1>
+      <p className="font-raleway text-white text-lg text-center max-w-2xl">
+        a
+      </p>
+    </div>
+  );
+}
