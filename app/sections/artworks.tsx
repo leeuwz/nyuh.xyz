3062bc257cf52d8a9454d570a4fb0f9484ce88b8
m <Jasperbos1234@gmail.com>
2025-08-23 01:21:39 +0200

    make artwork.tsx responsive

--- app/sections/artworks.tsx
+++ app/sections/artworks.tsx
@@ -1,6 +1,6 @@
 export default function Artworks() {
   return (
-    <div className="flex flex-col items-center justify-center mt-1 font-red-hat-display text-white text-[18px] tracking-[0.2rem] text-center mt-8">
+    <div className="flex flex-col items-center justify-center mt-1 font-red-hat-display text-white text-[14px] sm:text-[16px] md:text-[18px] tracking-[0.1rem] sm:tracking-[0.15rem] md:tracking-[0.2rem] text-center mt-4 md:mt-8 px-4">
       <p>for now go to my art twitter, i'll add something here at another time.. =w=</p>
       <p className="mt-2">art twitter: <a className="underline hover:text-[#F7395B] transition duration-300" href="https://x.com/sleepynyuh">@sleepynyuh</a></p>
     </div>
