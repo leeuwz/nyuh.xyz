4fc9faf338cdbb3881747ceae769ef1e5cc9d3ed
m <Jasperbos1234@gmail.com>
2025-08-22 23:32:13 +0200

    add artwork section page

---
+++ app/sections/artworks.tsx
@@ -0,0 +1,8 @@
+export default function Artworks() {
+  return (
+    <div className="flex flex-col items-center justify-center mt-1 font-red-hat-display text-white text-[18px] tracking-[0.2rem] text-center mt-8">
+      <p>for now go to my art twitter, i'll add something here at another time.. =w=</p>
+      <p className="mt-2">art twitter: <a className="underline hover:text-[#F7395B] transition duration-300" href="https://x.com/sleepynyuh">@sleepynyuh</a></p>
+    </div>
+  );
+}
