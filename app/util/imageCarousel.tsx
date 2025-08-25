778c891c58e9898b435e3205f0b6d2747c54cc4e
m <Jasperbos1234@gmail.com>
2025-08-26 00:58:05 +0200

    make sure all image carousels sync

--- app/util/imageCarousel.tsx
+++ app/util/imageCarousel.tsx
@@ -7,21 +7,20 @@ import { motion, AnimatePresence } from "motion/react";
 interface ImageCarouselProps {
   images: string[];
   skinTitle: string;
-  onImageClick: (image: string) => void;
+  onImageClick: () => void;
+  currentGlobalIndex?: number;
 }
 
-export default function ImageCarousel({ images, skinTitle, onImageClick }: ImageCarouselProps) {
+export default function ImageCarousel({ images, skinTitle, onImageClick, currentGlobalIndex = 0 }: ImageCarouselProps) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
     if (images.length <= 1) return;
-
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 7000); // Change image every 7 seconds
-
-    return () => clearInterval(interval);
-  }, [images.length]);
+    
+    // Use global index to sync all carousels
+    const newIndex = currentGlobalIndex % images.length;
+    setCurrentImageIndex(newIndex);
+  }, [currentGlobalIndex, images.length]);
 
   return (
     <div className="relative aspect-video cursor-pointer overflow-hidden rounded-lg">
@@ -33,7 +32,7 @@ export default function ImageCarousel({ images, skinTitle, onImageClick }: Image
           exit={{ opacity: 0 }}
           transition={{ duration: 1.0 }}
           className="absolute inset-0"
-          onClick={() => onImageClick(images[currentImageIndex])}
+          onClick={onImageClick}
         >
           <Image
             src={images[currentImageIndex]}
